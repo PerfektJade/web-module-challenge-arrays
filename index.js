b83d9148b30b73091ec091231ea23db141471386
Jade Coonts <perfektjade@gmail.com>
2022-08-09 17:58:29 -0400

    Task 1 completed.

--- index.js
+++ index.js
@@ -45,12 +45,10 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-
-function copy(/*your code here*/){
-  /*your code here*/
+function copy() {
+  return [...originalFlavors];
 }
-
-
+console.log('Task 1:', copy);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Confirm that an array is exactly 31 flavors. Your function should accept:
