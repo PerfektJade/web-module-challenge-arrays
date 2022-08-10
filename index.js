479806e7ef89b93b9c564c187933ede7705cb41b
Jade Coonts <perfektjade@gmail.com>
2022-08-10 04:01:20 -0400

    Task 7 completed.

--- index.js
+++ index.js
@@ -187,12 +187,20 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, string){
+    const filtered = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].includes(string)) {
+        filtered.push(array[i]);
+      }
+    }
+    return filtered;
 }
+console.log('Task 7:', filterByWord(originalFlavors, "Chocolate"));
 
-
+// function filterByWord(/*your code here*/){
+//   /*your code here*/
+// }
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
